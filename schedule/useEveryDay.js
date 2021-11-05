268174e3af9c818706bc89ac8da57f2fc61b0caa
wuhaoran <wuhaoran@kaikeba.com>
2021-11-05 11:42:36 +0800

    feat: edit description in every day say

--- schedule/useEveryDay.js
+++ schedule/useEveryDay.js
@@ -1,23 +1,6 @@
-// const { CronJob } = require('cron');
 const lib = require("../lib");
 const common = require("../common/index");
 const config = require("../wechat.config.js");
-
-// async function fundClock(bot) {
-//     return new CronJob(
-//         '01 30 14 * * 1-5',
-//         async () => {
-//             const rooms = await bot.Room.findAll({ topic: /对韭当割/ });
-//             const message = await getFunds();
-//             for (const room of rooms) {
-//                 await room.say(message);
-//             }
-//         },
-//         null,
-//         true,
-//         'Asia/Shanghai'
-//     );
-// }
 
 /**
  * 每日说定时任务设定，针对好友
@@ -27,6 +10,7 @@ const config = require("../wechat.config.js");
 async function setEveryDayTask(bot, item) {
     try {
         const { date: time, isRoom } = item;
+        const name = isRoom ? '群' : '用户';
         let contact;
         if (isRoom) {
             contact = await bot.Room.find({ topic: item.name }); // 获取你要发送的群组
@@ -37,11 +21,11 @@ async function setEveryDayTask(bot, item) {
         }
         if (!contact) {
             console.log(
-                `查找不到用户昵称为'${item.name}'或备注为'${item.alias}'的用户，请检查设置用户是否正确`
+                `查找不到${name}昵称为'${item.name}'或备注为'${item.alias}'的${name}，请检查设置 name 是否正确`
             );
             return;
         } else {
-            console.log(`设置用户：“${item.name || item.alias}”每日说任务成功`);
+            console.log(`${name}：“${item.name || item.alias}” 设置每日说任务成功`);
             lib.setSchedule(time, async () => {
                 let content = await common.getEveryDayContent(
                     item.memorialDay,
